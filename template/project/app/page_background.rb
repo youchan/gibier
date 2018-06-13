44610973d2bb66cb03eba25a6b2526d6eeed10ae
youchan <youchan01@gmail.com>
2018-06-13 11:44:32 +0900

    Delete customized code in page_background.rb

--- template/project/app/page_background.rb
+++ template/project/app/page_background.rb
@@ -2,33 +2,10 @@ class PageBackground
   include Hyalite::Component
 
   def background
-    case @props[:page_number] 
-    when 0
-      [
-        div({class: 'particleground'},
-          div({class: 'logo'}),
-          div({class: 'copyright'}, "© 2017 Retrieva, Inc.")
-        ),
-        div({class: 'vborder'})
-      ]
-    when Gibier.page_count - 1
-      [
-        div({class: 'particleground'},
-          div({class: 'logo large'}),
-          div({class: 'copyright'}, "© 2017 Retrieva, Inc.")
-        ),
-      ]
-    else
-      [
-        div({class: 'border'}),
-        div({class: 'dog'}),
-        div({class: 'copyright'}, "© 2017 Retrieva, Inc.")
-      ]
-    end
+    []
   end
 
   def render
     div({class: 'page-background', style: {zoom: @props[:zoom]}}, background)
   end
 end
-
